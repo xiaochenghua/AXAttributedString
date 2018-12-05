2041a9d0636c09c2159f4ddee8ff44bc813b74be
arnoldxiao <arnoldxiao@163.com>
2018-12-05 16:06:03 +0800

    Demo Code update!

--- AXAttributedStringDemo/AXAttributedStringDemo/AppDelegate.m
+++ AXAttributedStringDemo/AXAttributedStringDemo/AppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "AppDelegate.h"
+#import "ViewController.h"
 
 @interface AppDelegate ()
 
@@ -17,6 +18,13 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
+    [self.window makeKeyAndVisible];
+    
+    ViewController *vc = [[ViewController alloc] init];
+    UINavigationController *navigationController = [[UINavigationController alloc] initWithRootViewController:vc];
+    self.window.rootViewController = navigationController;
+    
     return YES;
 }
 
