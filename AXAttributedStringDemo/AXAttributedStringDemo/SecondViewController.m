a93d9db99382c21b10b708cf41d2f0700d8b4b38
arnoldxiao <arnoldxiao@163.com>
2018-12-05 18:18:34 +0800

    Update Test Code.

--- AXAttributedStringDemo/AXAttributedStringDemo/SecondViewController.m
+++ AXAttributedStringDemo/AXAttributedStringDemo/SecondViewController.m
@@ -23,7 +23,7 @@
 }
 
 - (void)dealloc {
-    NSLog(@"<<<<<<<<<<!!!delloc!!!<<<<<<<<<");
+    NSLog(@"<<<<<<<<<< delloc <<<<<<<<<");
 }
 
 - (UILabel *)attributedStringLabel {
@@ -35,10 +35,12 @@
             make.text(@"第一段").foregroundColor(UIColor.blackColor).font([UIFont systemFontOfSize:14]).underline(NSUnderlineStyleSingle);
             make.text(@"第二段").font([UIFont systemFontOfSize:14]).foregroundColor(UIColor.blueColor).backgroundColor(UIColor.lightGrayColor);
             make.text(@"第三段").foregroundColor(UIColor.orangeColor).font([UIFont boldSystemFontOfSize:20]).strike(NSUnderlineStyleThick).strikeColor(UIColor.orangeColor);
-            make.text(@"\n");
+            NSMutableParagraphStyle *paragraphStyle = [[NSMutableParagraphStyle alloc] init];
+            paragraphStyle.lineSpacing = 10;
+            make.text(@"\n").paragraphStyle(paragraphStyle);
 //            make.text(@"");
             make.text(@"第四段").foregroundColor(UIColor.brownColor).font([UIFont boldSystemFontOfSize:30]).baseline(-15);
-            make.text(@"第五段").foregroundColor(UIColor.redColor).systemFont(50);
+            make.text(@"第五段").foregroundColor(UIColorFromRGB(0xCC00FF)).systemFont(30);
         }];
     }
     return _attributedStringLabel;
