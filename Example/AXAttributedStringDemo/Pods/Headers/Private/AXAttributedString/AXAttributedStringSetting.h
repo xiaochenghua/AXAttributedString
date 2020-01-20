5ed7d328666bb83dbc893f36ad95501a7ac4e580
arnoldxiao <arnoldxiao@163.com>
2020-01-20 18:01:51 +0800

    Demo Pod创建

---
+++ Example/AXAttributedStringDemo/Pods/Headers/Private/AXAttributedString/AXAttributedStringSetting.h
@@ -0,0 +1,1 @@
+../../../../../../AXAttributedString/UI/AXAttributedStringSetting.h
---
+++ Example/AXAttributedStringDemo/Pods/Target Support Files/Pods-AXAttributedStringDemoTests/Pods-AXAttributedStringDemoTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_AXAttributedStringDemoTests : NSObject
+@end
+@implementation PodsDummy_Pods_AXAttributedStringDemoTests
+@end
